e37248713e6643f177aed7b5cc79919c06e023e4
IAshokGovind <iashokgovind1990@gmail.com>
2021-01-18 23:09:23 +0530

    Changes to docker-compose yml to run on port 80

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import { 
-  Container, 
-  Grid, 
-  NativeSelect, 
-  FormControl, 
-  Input} from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  NativeSelect,
+  FormControl,
+  Input
+} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import TableList from "../src/Components/Table";
 
@@ -39,40 +40,40 @@ function App() {
 
   useEffect(() => {
     console.log('I am called');
-    SetNumbers() ;
+    //SetNumbers();
   }, []);
 
   const SetNumbers = () => {
-      axios({
-        'method': 'GET',
-        'url': "https://cors-anywhere.herokuapp.com/http://www.randomnumberapi.com/api/v1.0/random?min=1&max=1000&count=2",
-        'headers': { "Access-Control-Allow-Origin": "http://localhost:3000" },
-        'params': {
-          'min': 1,
-          'max': 1000,
-          'count': 2
-        }
+    axios({
+      'method': 'GET',
+      'url': "https://cors-anywhere.herokuapp.com/http://www.randomnumberapi.com/api/v1.0/random?min=1&max=1000&count=2",
+      'headers': { "Access-Control-Allow-Origin": "http://localhost:3000" },
+      'params': {
+        'min': 1,
+        'max': 1000,
+        'count': 2
+      }
+    })
+      .then((response) => {
+        console.log("get Random Numbers" + response.data);
+        setState({
+          ...state,
+          number1: response.data[0],
+          number2: response.data[1]
+        });
       })
-        .then((response) => {
-          console.log("get Random Numbers" + response.data);
-          setState({
-            ...state,
-            number1: response.data[0],
-            number2: response.data[1]
-          });
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-        
-        console.log("check1")
-        Calc();
+      .catch((error) => {
+        console.log(error)
+      })
+
+    console.log("check1")
+    Calc(state.sign);
   }
 
-  const Calc = () => {
+  const Calc = (value) => {
     console.log(state)
     let output = 0;
-    switch (state.sign) {
+    switch (value) {
       case 1:
         output = (state.number1 + state.number2);
         break;
@@ -95,33 +96,30 @@ function App() {
   }
 
   const updateRow = () => {
-    rows.push(createData(rows.length+1, state.number1, state.sign, state.number2, state.output))
-    setState({
-      ...state,
-      rows: rows
-    })
+    localStorage.setItem('myData', createData(localStorage.length + 1, state.number1,
+      state.sign, state.number2, state.output))
   }
-  
-function createData(id, number1, sign, number2, output) {
+
+  function createData(id, number1, sign, number2, output) {
     let sign1 = switchSign(sign);
     console.log(id + " , " + number1 + " , " + sign1 + " , " + number2 + " , " + output)
     return { id, number1, sign1, number2, output };
-}
+  }
 
-function switchSign(sign) {
+  function switchSign(sign) {
     switch (sign) {
-        case 1:
-            return "+";
-        case 2:
-            return "-";
-        case 3:
-            return "x";
-        case 4:
-            return ":";
-        default:
-            break;
+      case 1:
+        return "+";
+      case 2:
+        return "-";
+      case 3:
+        return "x";
+      case 4:
+        return ":";
+      default:
+        break;
     }
-}
+  }
 
   return (
     <div style={{ marginTop: 20, padding: 30 }}>
@@ -135,17 +133,19 @@ function switchSign(sign) {
             <Input id="number1" type="text" value={state.number1} onChange={handleChange} />
           </FormControl>
 
-          <NativeSelect 
-            name="sign"
-            onChange={handleChange} 
-            value={state.sign}
-            inputProps={{ 'sign-id': 'sign' }}>
-            <option value={0}></option>
-            <option value={1}>+</option>
-            <option value={2}>-</option>
-            <option value={3}>x</option>
-            <option value={4}>:</option>
-          </NativeSelect>
+          <FormControl>
+            <NativeSelect
+              name="sign"
+              onChange={handleChange}
+              value={state.sign}
+              inputProps={{ 'sign-id': 'sign' }}>
+              <option value={0}></option>
+              <option value={1}>+</option>
+              <option value={2}>-</option>
+              <option value={3}>x</option>
+              <option value={4}>:</option>
+            </NativeSelect>
+          </FormControl>
 
           <FormControl>
             <Input id="number2" type="text" value={state.number2} onChange={handleChange} />
